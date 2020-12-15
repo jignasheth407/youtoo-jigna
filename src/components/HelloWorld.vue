2a7ffe3ed1f8a93329e8ec4687b4ea32b253e259
Navneet <navneet.brinfotech@gmail.com>
2020-12-15 18:39:00 +0530

    init commit jigna

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,401 @@
+<template>
+  <div  >
+ <div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Groups</a>
+      </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <!-- <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form> -->
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">      
+      <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-bell"></i>
+          <span class="badge badge-warning navbar-badge">15</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-header">15 Notifications</span>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-envelope mr-2"></i> 4 new messages
+            <span class="float-right text-muted text-sm">3 mins</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> 8 friend requests
+            <span class="float-right text-muted text-sm">12 hours</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-file mr-2"></i> 3 new reports
+            <span class="float-right text-muted text-sm">2 days</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <div class="user-panel d-flex float-left">
+            <div class="image">
+              <img src="../assets/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+              <a href="#" class="d-block">Slave N.</a>
+            </div>            
+          </div>
+          <a class="nav-link float-left" data-toggle="dropdown" href="#">
+              <i class="right fas fa-angle-down"></i>
+            </a>
+          
+        <div class="dropdown-menu dropdown-menu-md dropdown-menu-right">          
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> My Profile            
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> Group            
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-file mr-2"></i> Logout            
+          </a>
+          
+          
+        </div>
+      </li>
+      
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-light-primary elevation-4">
+    <!-- Brand Logo -->
+    <!-- <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a> -->
+    <a href="#" class="brand-link">
+      
+      <img src="../assets/img/youto-logo2.jpg" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">
+
+
+
+      <span class="brand-text font-weight-light"><img src="../assets/img/youto-logo.jpg" alt="AdminLTE Logo" class="brand-image"></span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">     
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="#" class="nav-link active"><i class="nav-icon fa fa-plus"></i> Create</a>           
+          </li> 
+          <li class="nav-item has-treeview ">
+            <a href="#" class="nav-link">
+              <!-- <i class="nav-icon fas fa-tachometer-alt"></i> -->
+              <p>
+                My groups
+                <i class="right fas fa-angle-up"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <!-- <i class="far fa-circle nav-icon"></i> -->
+                  <input type="checkbox" name="">&nbsp;&nbsp;
+                  <p>My partners</p>
+                  <i class="right fas fa-pen"></i>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <!-- <i class="far fa-circle nav-icon"></i> -->
+                  <input type="checkbox" name="">&nbsp;&nbsp;
+                  <p>My partners</p>
+                  <i class="right fas fa-pen"></i>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <!-- <i class="far fa-circle nav-icon"></i> -->
+                  <input type="checkbox" name="">&nbsp;&nbsp;
+                  <p>My partners</p>
+                  <i class="right fas fa-pen"></i>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <!-- <i class="far fa-circle nav-icon"></i> -->
+                  <input type="checkbox" name="">&nbsp;&nbsp;
+                  <p>My partners</p>
+                  <i class="right fas fa-pen"></i>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <!-- <i class="far fa-circle nav-icon"></i> -->
+                  <input type="checkbox" name="">&nbsp;&nbsp;
+                  <p>My partners</p>
+                  <i class="right fas fa-pen"></i>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <!-- <i class="far fa-circle nav-icon"></i> -->
+                  <input type="checkbox" name="">&nbsp;&nbsp;
+                  <p>My partners</p>
+                  <i class="right fas fa-pen"></i>
+                </a>
+              </li>
+            </ul>
+          </li>    
+          
+          <li class="nav-item">
+            <div class="dropdown-divider"></div>
+          </li>
+           <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard                
+              </p>
+            </a>           
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-calendar-alt"></i>
+              
+              <p>
+                Events
+                <!-- <span class="right badge badge-danger">New</span> -->
+              </p>
+            </a>
+          </li>
+           <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-comment-alt"></i>
+              
+              <p>
+                Conversation
+                <!-- <span class="right badge badge-danger">New</span> -->
+              </p>
+            </a>
+          </li>
+                 <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-user-alt"></i>
+              
+              <p>
+                Members
+                <!-- <span class="right badge badge-danger">New</span> -->
+              </p>
+            </a>
+          </li>
+                <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-users"></i>
+              
+              <p>
+               Groups
+                <!-- <span class="right badge badge-danger">New</span> -->
+              </p>
+            </a>
+          </li>
+                <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-align-justify"></i>
+              
+              <p>
+                Reports
+                <!-- <span class="right badge badge-danger">New</span> -->
+              </p>
+            </a>
+          </li>
+        
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">My Groups</h1>
+          </div><!-- /.col -->
+          <!-- <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div> --><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-3">
+            <div class="card">
+              <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Group+Image" alt="Group image cap">
+              <div class="card-body">
+                <h5 class="card-title" style="float: inherit;">Nisman Team Building</h5><br>               
+
+                <a href="#" class="card-link float-left">13 members</a>
+                <a href="#" class="card-link float-right"><i class="nav-icon fas fa-calendar-alt"></i></a>
+              </div>
+            </div>            
+          </div>
+           <div class="col-lg-3">
+              <div class="card">
+                <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Group+Image" alt="Group image cap">
+                <div class="card-body">
+                  <h5 class="card-title" style="float: inherit;">Nisman Team Building</h5><br>               
+
+                  <a href="#" class="card-link float-left">13 members</a>
+                  <a href="#" class="card-link float-right"><i class="nav-icon fas fa-calendar-alt"></i></a>
+                </div>
+              </div>
+              <!-- /.card -->
+           </div>
+           <div class="col-lg-3">
+              <div class="card">
+                <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Group+Image" alt="Group image cap">
+                <div class="card-body">
+                  <h5 class="card-title" style="float: inherit;">Nisman Team Building</h5><br>               
+
+                  <a href="#" class="card-link float-left">13 members</a>
+                  <a href="#" class="card-link float-right"><i class="nav-icon fas fa-calendar-alt"></i></a>
+                </div>
+              </div><!-- /.card -->
+           </div>
+           <div class="col-lg-3">
+            <div class="card">
+              <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Group+Image" alt="Group image cap">
+              <div class="card-body">
+                <h5 class="card-title" style="float: inherit;">Nisman Team Building</h5><br>               
+
+                <a href="#" class="card-link float-left">13 members</a>
+                <a href="#" class="card-link float-right"><i class="nav-icon fas fa-calendar-alt"></i></a>
+              </div>
+            </div>            
+          </div>
+           <div class="col-lg-3">
+              <div class="card">
+                <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Group+Image" alt="Group image cap">
+                <div class="card-body">
+                  <h5 class="card-title" style="float: inherit;">Nisman Team Building</h5><br>               
+
+                  <a href="#" class="card-link float-left">13 members</a>
+                  <a href="#" class="card-link float-right"><i class="nav-icon fas fa-calendar-alt"></i></a>
+                </div>
+              </div>
+              <!-- /.card -->
+           </div>
+           <div class="col-lg-3">
+              <div class="card">
+                <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Group+Image" alt="Group image cap">
+                <div class="card-body">
+                  <h5 class="card-title" style="float: inherit;">Nisman Team Building</h5><br>               
+
+                  <a href="#" class="card-link float-left">13 members</a>
+                  <a href="#" class="card-link float-right"><i class="nav-icon fas fa-calendar-alt"></i></a>
+                </div>
+              </div><!-- /.card -->
+           </div>
+           <div class="col-lg-3">
+            <div class="card">
+              <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Group+Image" alt="Group image cap">
+              <div class="card-body">
+                <h5 class="card-title" style="float: inherit;">Nisman Team Building</h5><br>               
+
+                <a href="#" class="card-link float-left">13 members</a>
+                <a href="#" class="card-link float-right"><i class="nav-icon fas fa-calendar-alt"></i></a>
+              </div>
+            </div>            
+          </div>
+           <div class="col-lg-3">
+              <div class="card">
+                <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Group+Image" alt="Group image cap">
+                <div class="card-body">
+                  <h5 class="card-title" style="float: inherit;">Nisman Team Building</h5><br>               
+
+                  <a href="#" class="card-link float-left">13 members</a>
+                  <a href="#" class="card-link float-right"><i class="nav-icon fas fa-calendar-alt"></i></a>
+                </div>
+              </div>
+              <!-- /.card -->
+           </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      <small>
+        <a href="#">Privacy</a>&nbsp;&nbsp;<a href="#">Terms</a>&nbsp;&nbsp;<a href="#">Help</a>
+      </small>   
+    </div>
+    <!-- Default to the left -->
+    <small>Made by &nbsp;<i class="fas fa-heart"></i>&nbsp; <a href="/">Nisman Solutions</a>.</small> 
+  </footer>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
